5831883c8dd8b2e664af626443c0275fb3538557
CLackrone <charisaanne@gmail.com>
2018-02-08 14:58:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Code goes here
+import React from 'react';
+
+import { message } from './components/foo';
+import Tweet from './Tweet';
+import ReactDOM from 'react-dom';
+
+import fruit from './fruits';
+console.log(fruit.apple);
+
+
+ 
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
